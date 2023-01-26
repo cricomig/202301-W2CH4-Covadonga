d2440a877fa7b854bbcbbb68f85d8c87875f0664
Covadonga M <cova.millan@gmail.com>
2023-01-26 19:47:19 +0100

    end some function

--- functions.test.js
+++ functions.test.js
@@ -1,10 +1,17 @@
-import { length, push, pop, shift, unshift } from './functions.js';
+import { length, push, pop, shift, unshift, some } from './functions.js';
 
 describe('Given length function', () => {
   describe('When we have an Array', () => {
     test('Then if array`s lenght is 5 the result should be 5', () => {
       const arrayTest = [1, 'pepe', true, 0, 'Cova'];
       const expected = 5;
+      const result = length(arrayTest);
+      expect(result).toBe(expected);
+    });
+
+    test('Then if array`s lenght is 5 the result should be 5', () => {
+      const arrayTest = [];
+      const expected = 0;
       const result = length(arrayTest);
       expect(result).toBe(expected);
     });
@@ -20,6 +27,14 @@ describe('Given push function', () => {
       const result = push(arrayTest, element);
       expect(result).toBe(expected);
     });
+
+    test('Then the element adds to the array and the length grows n-element', () => {
+      const arrayTest = [];
+      const element = 'Leandro';
+      const expected = 1;
+      const result = push(arrayTest, element);
+      expect(result).toBe(expected);
+    });
   });
 });
 
@@ -28,6 +43,13 @@ describe('Given pop function', () => {
     test('Then the last element will be deleted will be returned', () => {
       const arrayTest = [1, 'pepe', true, 0, 'Cova'];
       const expected = 'Cova';
+      const result = pop(arrayTest);
+      expect(result).toBe(expected);
+    });
+
+    test('Then the last element will be deleted will be returned', () => {
+      const arrayTest = [];
+      const expected = undefined;
       const result = pop(arrayTest);
       expect(result).toBe(expected);
     });
@@ -42,6 +64,13 @@ describe('Given shift function', () => {
       const result = shift(arrayTest);
       expect(result).toBe(expected);
     });
+
+    test('Then the first element will be deleted will be returned', () => {
+      const arrayTest = [];
+      const expected = undefined;
+      const result = shift(arrayTest);
+      expect(result).toBe(expected);
+    });
   });
 });
 
@@ -52,6 +81,34 @@ describe('Given unshift function', () => {
       const element = 'Leandro';
       const expected = 6;
       const result = unshift(arrayTest, element);
+      expect(result).toBe(expected);
+    });
+
+    test('Then n-element will be added to the beginning of the Array', () => {
+      const arrayTest = [];
+      const element = 'Leandro';
+      const expected = undefined;
+      const result = unshift(arrayTest, element);
+      expect(result).toBe(expected);
+    });
+  });
+});
+
+describe('Given some function', () => {
+  describe('When we have an Array and a condition', () => {
+    test('Then if at least one element match with the condition, return true, else false', () => {
+      const arrayTest = [1, 'pepe', true, 0, 'Cova'];
+      const condition = 'Cova';
+      const expected = true;
+      const result = some(arrayTest, condition);
+      expect(result).toBe(expected);
+    });
+
+    test('Then if at least one element match with the condition, return true, else false', () => {
+      const arrayTest = [];
+      const condition = 'Cova';
+      const expected = undefined;
+      const result = some(arrayTest, condition);
       expect(result).toBe(expected);
     });
   });
